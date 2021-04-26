92d371b5684fe68960ebf57c3370c57407ae051a
Fredrik Johansson <jeanfredrik@gmail.com>
2021-04-26 17:33:02 +0200

    feat: set explicit names for acf field groups
    Sets explicit field group names for these module types:
    - Contacts
    - Gallery
    - Iframe
    - RSS
    - Table
    - Video

--- autoload/field-groups.php
+++ autoload/field-groups.php
@@ -15,17 +15,23 @@ function set_acf_group_graphql_field_name($key, $name) {
   });
 }
 
-// Text
-set_acf_group_graphql_field_name('group_5891b49127038', 'modTextOptions');
+// Contacts
+set_acf_group_graphql_field_name('group_5805e5dc0a3be', 'modContactsOptions');
 
-// Notice
-set_acf_group_graphql_field_name('group_575a842dd1283', 'modNoticeOptions');
+// Fileslist
+set_acf_group_graphql_field_name('group_5756ce3e48783', 'modFileslistOptions');
+
+// Gallery
+set_acf_group_graphql_field_name('group_5666af6d26b7c', 'modGalleryOptions');
+
+// Iframe
+set_acf_group_graphql_field_name('group_56c47016ea9d5', 'modIframeOptions');
 
 // Image
 set_acf_group_graphql_field_name('group_570770ab8f064', 'modImageOptions');
 
-// Fileslist
-set_acf_group_graphql_field_name('group_5756ce3e48783', 'modFileslistOptions');
+// Notice
+set_acf_group_graphql_field_name('group_575a842dd1283', 'modNoticeOptions');
 
 // Posts
 set_acf_group_graphql_field_name('group_571dfd3c07a77', 'modPostsDataDisplay');
@@ -35,3 +41,15 @@ set_acf_group_graphql_field_name(
 );
 set_acf_group_graphql_field_name('group_571dffc63090c', 'modPostsDataSorting');
 set_acf_group_graphql_field_name('group_571dfaabc3fc5', 'modPostsDataSource');
+
+// RSS
+set_acf_group_graphql_field_name('group_59535d940706c', 'modRssOptions');
+
+// Table
+set_acf_group_graphql_field_name('group_5666a2a71d806', 'modTableOptions');
+
+// Text
+set_acf_group_graphql_field_name('group_5891b49127038', 'modTextOptions');
+
+// Video
+set_acf_group_graphql_field_name('group_57454ae7b0e9a', 'modVideoOptions');
