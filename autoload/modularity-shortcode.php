d74fbaaa4e89a91892a8bb62c93b216074a88680
Fredrik Johansson <jeanfredrik@gmail.com>
2022-10-18 20:34:52 +0200

    feat: Allow module shortcodes inside modules

--- autoload/modularity-shortcode.php
+++ autoload/modularity-shortcode.php
@@ -7,6 +7,37 @@ use WPGraphQL\Data\Connection\PostObjectConnectionResolver;
 use WPGraphQL\Data\Connection\TermObjectConnectionResolver;
 use WPGraphQL\Model\Post as PostModel;
 
+function modularity_graphql_extract_content_module_ids($content) {
+  /**
+   * Copied from `do_shortcode`
+   */
+  // Find all registered tag names in $content.
+  preg_match_all('@\[([^<>&/\[\]\x00-\x20=]++)@', $content, $matches);
+  $tagnames = array_intersect(["modularity"], $matches[1]);
+  if (empty($tagnames)) {
+    return null;
+  }
+  $pattern = get_shortcode_regex($tagnames);
+  $module_ids = [];
+  preg_match_all("/$pattern/", $content, $matches, PREG_SET_ORDER);
+
+  /**
+   * Copied from `do_shortcode_tag`
+   */
+  foreach ($matches as $m) {
+    // Allow [[foo]] syntax for escaping a tag.
+    if ("[" === $m[1] && "]" === $m[6]) {
+      continue;
+    }
+    $attr = shortcode_parse_atts($m[3]);
+    if (!empty($attr["id"])) {
+      $module_ids[] = $attr["id"];
+    }
+  }
+
+  return $module_ids;
+}
+
 /**
  * Replaces the original handler for the `[modularity]` shortcode so that it
  * works better with the HTML processor in Gatsby.
@@ -46,32 +77,7 @@ add_action("graphql_register_types", function ($type_registry) {
       }
       $content = $post->post_content;
 
-      /**
-       * Copied from `do_shortcode`
-       */
-      // Find all registered tag names in $content.
-      preg_match_all('@\[([^<>&/\[\]\x00-\x20=]++)@', $content, $matches);
-      $tagnames = array_intersect(["modularity"], $matches[1]);
-      if (empty($tagnames)) {
-        return null;
-      }
-      $pattern = get_shortcode_regex($tagnames);
-      $module_ids = [];
-      preg_match_all("/$pattern/", $content, $matches, PREG_SET_ORDER);
-
-      /**
-       * Copied from `do_shortcode_tag`
-       */
-      foreach ($matches as $m) {
-        // Allow [[foo]] syntax for escaping a tag.
-        if ("[" === $m[1] && "]" === $m[6]) {
-          continue;
-        }
-        $attr = shortcode_parse_atts($m[3]);
-        if (!empty($attr["id"])) {
-          $module_ids[] = $attr["id"];
-        }
-      }
+      $module_ids = modularity_graphql_extract_content_module_ids($content);
 
       $resolver = new PostObjectConnectionResolver(
         $parent,
@@ -87,3 +93,38 @@ add_action("graphql_register_types", function ($type_registry) {
     },
   ]);
 });
+
+add_action(
+  "graphql_register_types",
+  function ($type_registry) {
+    $type_registry->register_connection([
+      "fromType" => "ModPosts_Modpostsdatasource_data",
+      "toType" => "ContentNode",
+      "fromFieldName" => "postContentModularityModules",
+      "resolve" => function (
+        $parent,
+        $args,
+        AppContext $context,
+        ResolveInfo $info
+      ) {
+        $content = $parent["field_57625914110b2"];
+
+        $module_ids = modularity_graphql_extract_content_module_ids($content);
+
+        $resolver = new PostObjectConnectionResolver(
+          $parent,
+          $args,
+          $context,
+          $info,
+          "any"
+        );
+        $resolver->set_query_arg("post__in", $module_ids);
+        $resolver->set_query_arg("orderby", "post__in");
+
+        return null !== $resolver ? $resolver->get_connection() : null;
+      },
+    ]);
+  },
+  10,
+  1
+);
